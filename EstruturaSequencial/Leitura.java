a44d1a8056c7f73b03c2124be8beea032837e673
luiz_lelis <luiz.lelis@spyonsky.com.br>
2022-01-10 11:46:07 -0300

    Leitura com Scanner

--- EstruturaSequencial/Leitura.java
+++ EstruturaSequencial/Leitura.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 
 public class Leitura {
     public static void main(String[] args) {  
+        //Declaração da entrada de dados
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite seu Nome: ");
+        //Gravando os dados na variavel
         String nome = sc.nextLine();
         System.out.println("Digite sua Idade: ");
         int idade = sc.nextInt();
@@ -13,12 +15,12 @@ public class Leitura {
         Double altura = sc.nextDouble();
         System.out.println("Tamanho de Camisa: ");
         char tamBlusa = sc.next().charAt(0);
-
+        //Impressões com println
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         System.out.println("Altura: " + altura);
         System.out.println("Tamanho Blusa: " + tamBlusa);
-
+        //fechando a entrada de Dados
         sc.close();
     }
 }
