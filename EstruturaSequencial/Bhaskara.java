f515e34da279fb1873a502bdf6cde01e8d2f46ac
luiz_lelis <luiz.lelis@spyonsky.com.br>
2022-01-10 11:55:25 -0300

    Calcular Bhaskara

--- EstruturaSequencial/Bhaskara.java
+++ EstruturaSequencial/Bhaskara.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bhaskara {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        //Pegando os Valores do Usuario.
         System.out.println("**********Calcular Delta**********");
         System.out.printf("Valor de A: ");
         Double a = sc.nextDouble();
@@ -13,13 +13,13 @@ public class Bhaskara {
         Double b = sc.nextDouble();
         System.out.printf("Valor de C: ");
         Double c = sc.nextDouble();
-
+        //fechando Scanner
         sc.close();
-
+        //Fazendo o uso da biblioteca Math para valor elevado
         Double delta = (Math.pow(b, 2.0) - (4*a*c));
 
         System.out.println("Delta: " + delta);
-
+        //Fazedo o calculo e salvando nas variaveis usando sqrt para raiz quadrada.
         System.out.println("\n**********Calcular Bhaskara**********\n");
         Double x1 = ((-b + Math.sqrt(delta))/(2.0 * a));
         Double x2 = ((-b - Math.sqrt(delta))/(2.0 * a));
