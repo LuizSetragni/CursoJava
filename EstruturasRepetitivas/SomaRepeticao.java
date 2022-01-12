a4dbc84e53a5eb626e9998ba6cb93d9caba3d757
luiz_lelis <luiz.lelis@spyonsky.com.br>
2022-01-12 11:39:21 -0300

    Repeticoes

---
+++ EstruturasRepetitivas/SomaRepeticao.java
@@ -0,0 +1,20 @@
+package EstruturasRepetitivas;
+import java.util.*;
+public class SomaRepeticao {
+    public static void main(String[] args) {
+        
+        int x = -1, y = 0;
+
+        
+        Scanner sc = new Scanner(System.in);
+
+        while(x != 0){
+            System.out.println("Digite um numero (Exit - 0)");
+            x = sc.nextInt();
+            y += x;
+        }
+        System.out.printf("Resultado: %d", y);
+        sc.close();
+    }
+    
+}
